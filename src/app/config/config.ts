87b2bde98f83ac7364c2b2312b887eee05b13934
Maxikk26 <maximiliano.bogo@gmail.com>
2020-07-13 16:47:19 -0400

    MOD ruta api con ssl

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 // export const URL_SERVICIOS = 'http://localhost:5000/api';
-export const URL_SERVICIOS = 'http://monyucab-aempresa.somee.com/api';
+export const URL_SERVICIOS = 'https://monyucab-aempresa.somee.com/api';
 //export const URL_SERVICIOS = 'https://66.42.95.58/api';
